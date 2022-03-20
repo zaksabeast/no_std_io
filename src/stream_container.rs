593d1103bd5a0088345fa282ad3a4eb7af4f21bd
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2022-03-20 08:23:29 -0500

    Grow vector StreamContainers

--- src/stream_container.rs
+++ src/stream_container.rs
@@ -1,4 +1,4 @@
-use crate::{Cursor, Reader, Writer};
+use crate::{Cursor, Reader, Writer, WriterResult};
 
 pub struct StreamContainer<T: Reader> {
     raw: T,
@@ -25,6 +25,10 @@ impl<T: Reader + Writer> Writer for StreamContainer<T> {
     fn get_mut_slice(&mut self) -> &mut [u8] {
         self.raw.get_mut_slice()
     }
+
+    fn get_sized_mut_slice(&mut self, offset: usize, length: usize) -> WriterResult<&mut [u8]> {
+        self.raw.get_sized_mut_slice(offset, length)
+    }
 }
 
 impl<T: Reader> Cursor for StreamContainer<T> {
@@ -40,7 +44,7 @@ impl<T: Reader> Cursor for StreamContainer<T> {
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::StreamReader;
+    use crate::{StreamReader, StreamWriter};
     use alloc::vec;
 
     #[test]
@@ -59,5 +63,21 @@ mod test {
     fn should_work_with_mut_slices() {
         let mut data: [u8; 4] = [0xaa, 0xbb, 0xcc, 0xdd];
         StreamContainer::new(data.as_mut_slice()).default_read_stream::<u32>();
+    }
+
+    #[test]
+    fn should_grow_a_vector_if_needed() {
+        let data = vec![];
+        let mut stream = StreamContainer::new(data);
+        stream.checked_write_stream_bytes(&[0xaa, 0xbb, 0xcc, 0xdd]);
+        assert_eq!(stream.into_raw(), [0xaa, 0xbb, 0xcc, 0xdd]);
+    }
+
+    #[test]
+    fn should_not_grow_a_vector_if_not_needed() {
+        let data = vec![0; 4];
+        let mut stream = StreamContainer::new(data);
+        stream.checked_write_stream_bytes(&[0xaa, 0xbb, 0xcc, 0xdd]);
+        assert_eq!(stream.into_raw(), [0xaa, 0xbb, 0xcc, 0xdd]);
     }
 }

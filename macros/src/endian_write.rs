cbecba9bd26ae89f32434786fc02cf5a50b77f3b
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2022-03-29 07:19:40 -0500

    Use explicit error

--- macros/src/endian_write.rs
+++ macros/src/endian_write.rs
@@ -33,7 +33,7 @@ fn create_write_method_impl(
         .collect::<Vec<proc_macro2::TokenStream>>();
 
     quote! {
-      fn #impl_method(&self, dst: &mut [u8]) -> Result<usize, Error> {
+      fn #impl_method(&self, dst: &mut [u8]) -> Result<usize, ::no_std_io::Error> {
         let mut stream = ::no_std_io::StreamContainer::new(dst);
         #(#field_tokens)*
         let bytes_written = ::no_std_io::Cursor::get_index(&stream);
